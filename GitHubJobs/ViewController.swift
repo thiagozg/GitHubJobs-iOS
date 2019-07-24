c013b81184ace943d44c5ee05ce3ef55a9691898
Thiago Zagui Giacomini <thiagozg1995@gmail.com>
2019-07-24 19:47:27 -0300

    Initial Commit

---
+++ GitHubJobs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubJobs
+//
+//  Created by Thiago Zagui Giacomini on 24/07/19.
+//  Copyright © 2019 Thiago Zagui Giacomini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
